f761b6bc5ccb5b9f1de879cad54ad0a9a190864f
hossam <hossam@hossam.com>
2021-12-19 16:02:45 +0200

    3 19-12-2021 4:01 pm

--- home/hmuwy1m8l95t/myapp/myapp/views.py
+++ home/hmuwy1m8l95t/myapp/myapp/views.py
@@ -1,10 +1,13 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from datetime import datetime
-import pytz
+# import pytz
+from django.shortcuts import render
+from .forms import contact_form
 
 
 def index(request):
-    date_1 = str(datetime.now(pytz.timezone('egypt')))
-    msg = "HI ya hossam this first page at first Django Project at Godday "+date_1
-    return HttpResponse(msg)
+    # date_1 = str(datetime.now(pytz.timezone('egypt')))
+    # msg = "HI ya hossam this first page at first Django Project at Godday "+date_1
+    html_page = 'myapp/index.html'
+    return render(request, html_page, {'contact_form': contact_form})
     # return HttpResponse("asd asd asd")
