adf7cb54668f1386fa11df56ce29a9678c02f3c3
hossamalhossany <hossam.2030@gmail.com>
2021-12-24 22:21:33 +0200

    26 *** 24-12-2021 ** 10:30 PM-home

--- home/hmuwy1m8l95t/myapp/myapp/urls.py
+++ home/hmuwy1m8l95t/myapp/myapp/urls.py
@@ -1,11 +1,19 @@
-from django.urls import path
+from django.template.defaulttags import url
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('', views.home),
     path('', views.index, name='index'),
-    # here this issue # hossam
     path('put_data_to_databasa', views.put_data_to_databasa, name='put_data_to_databasa'),
-# here this issue # hossam
-    path("back_to_hossamweb", views.back_to_hossamweb, name='back_to_hossamweb')
+    path("myapp.hossamweb.com/back_to_hossamweb/", views.back_to_hossamweb, name='back_to_hossamweb'),
+    # url("back_to_hossamweb", views.back_to_hossamweb),
+
+    # path('', views.home),
+
+    # here this issue # hossam
+
+# # here this issue # hossam
+
+# #     url(r'(?P<subdomain>[a-z]+)/sigin/$', 'view'),
+#     url('myapp.hossamweb.com/back_to_hossamweb/', views.back_to_hossamweb),
 ]
