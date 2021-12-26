b8603020042d665c77f6ed23b5538f4a035538e0
hossam <hossam@hossam.com>
2021-12-26 13:22:47 +0200

    30  **** 26-12-2021 1:22 pm -- work

--- home/hmuwy1m8l95t/myapp/myapp/urls.py
+++ home/hmuwy1m8l95t/myapp/myapp/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('put_data_to_databasa', views.put_data_to_databasa, name='put_data_to_databasa'),
     path(r"^back_to_hossamweb/$", views.back_to_hossamweb, name='back_to_hossamweb'),
     # path(r'back_to_google/', views.back_to_google, name='back_to_google'),
-    # url(r'(?P<subdomain>[a-z]+)/sigin/$', 'view'),
+    # url(r'(?P<subdomain>[a-z]+)/sigin/$', 'view'),   ###
 ]
