d5ab3aeb9048999c40c02858a55b7900e543878d
hossamalhossany <hossam.2030@gmail.com>
2021-12-26 21:47:23 +0200

    36 *** 26-12-2021 ** 9:30 PM-home

--- home/hmuwy1m8l95t/myapp/myapp/urls.py
+++ home/hmuwy1m8l95t/myapp/myapp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', admin.site.urls),
     path('back_to_google', views.back_to_google),
     path('put_data_to_databasa', views.put_data_to_databasa, name='put_data_to_databasa'),
-    # path('<myapp.hossamweb.com>/back_to_hossamweb/', views.put_data_to_databasa, name='back_to_hossamweb'),
+    # path('<myapp.hossamweb.com>/back_to_hossamweb/', views.put_data_to_databasa, name='back_to_hossamweb'),#
     path('back_to_hossamweb/', views.put_data_to_databasa, name='back_to_hossamweb'),
    ]
 
